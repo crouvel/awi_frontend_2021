2047cd944c29d3f0448cdfe8936bb706830e5d24
crouvel <55972660+crouvel@users.noreply.github.com>
2021-11-03 00:21:08 +0100

    ingredient list empty added

---
+++ src/components/IngredientList/IngredientList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './IngredientList.css';
+
+class IngredientList extends Component {
+
+    render() {
+        return(
+            <div>Liste ingredients par catégorie</div>
+        )
+    }
+
+}
+
+export default IngredientList;
