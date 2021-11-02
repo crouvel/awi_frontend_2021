597621afd092f2c6dbc38220bdc9cbbc36728534
crouvel <55972660+crouvel@users.noreply.github.com>
2021-11-02 23:36:34 +0100

    add mercurials cards + change font family

--- src/components/Mercurials/Mercurials.js
+++ src/components/Mercurials/Mercurials.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import './Mercurials.css';
+import IngCategorycard from '../IngredientCategoryCard/IngCategorycard';
 
 class Mercurials extends Component {
 
     render() {
         return(
-            <div>mercurials</div>
+            <div>mercurials
+                <IngCategorycard/>
+            </div>
         )
     }
 
