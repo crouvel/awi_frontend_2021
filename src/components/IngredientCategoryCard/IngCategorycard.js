f604b57ce28309efe80ef570b33f81265a5bced1
crouvel <55972660+crouvel@users.noreply.github.com>
2021-11-04 19:50:19 +0100

    try to fix 1

--- src/components/IngredientCategoryCard/IngCategorycard.js
+++ src/components/IngredientCategoryCard/IngCategorycard.js
@@ -21,59 +21,76 @@ const IngredientCategories = () => {
 
    console.log(categoriesIngredientList.data);
  
+   const showData = () => {
+    if(categoriesIngredientList.data != null) {
+        return (
+          <div class="main-container">
+          <div class="cards">
+            <div class="card card-1">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">{categoriesIngredientList.data[0].libellé}</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+            <div class="card card-2">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+            <div class="card card-3">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">Ut enim ad minim veniam.</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+            <div class="card card-4">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">Quis nostrud exercitation ullamco laboris nisi.</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+            <div class="card card-5">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">Ut aliquip ex ea commodo consequat. Duis aute irure dolor.</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+            <div class="card card-1">
+              <div class="card__icon"><i class="fas fa-bolt"></i></div>
+              <p class="card__exit"><i class="fas fa-times"></i></p>
+              <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+              <p class="card__apply">
+                <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
+              </p>
+            </div>
+          </div>
+        </div> 
+        )
+    }
+    if(categoriesIngredientList.loading) {
+        return <p>loading</p>;
+    }
+    if(categoriesIngredientList.errorMsg !== "") {
+        return <p>{categoriesIngredientList.errorMsg}</p>;
+    }
+
+    return <p>Impossible d'obtenir des données</p>;
+};
         return(
-            <div class="main-container">
-  <div class="cards">
-    <div class="card card-1">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title"></h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-    <div class="card card-2">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-    <div class="card card-3">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title">Ut enim ad minim veniam.</h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-    <div class="card card-4">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title">Quis nostrud exercitation ullamco laboris nisi.</h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-    <div class="card card-5">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title">Ut aliquip ex ea commodo consequat. Duis aute irure dolor.</h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-    <div class="card card-1">
-      <div class="card__icon"><i class="fas fa-bolt"></i></div>
-      <p class="card__exit"><i class="fas fa-times"></i></p>
-      <h2 class="card__title">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
-      <p class="card__apply">
-        <a class="card__link" href="#">Apply Now <i class="fas fa-arrow-right"></i></a>
-      </p>
-    </div>
-  </div>
-</div>
+          <div>
+          {showData()}
+          </div>
         )
     
 
