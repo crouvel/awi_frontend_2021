fca4c9fda5aa12fe889cdebe71ec00e0d6f2e304
crouvel <55972660+crouvel@users.noreply.github.com>
2021-11-22 22:59:36 +0100

    table displayed ingredients by category 1

--- src/components/IngredientList/IngredientList.js
+++ src/components/IngredientList/IngredientList.js
@@ -4,49 +4,50 @@ import { useDispatch, useSelector } from "react-redux";
 import Loading from '../Loading/Loading';
 import {
     useParams
-  } from "react-router-dom";
+} from 'react-router-dom';
 import './IngredientList.css';
-import {getIngredientByCategory} from '../../actions/IngredientsByCategoryAction';
+import { getIngredientByCategory } from '../../actions/IngredientsByCategoryAction';
+import ReactFlexyTable from 'react-flexy-table';
+import 'react-flexy-table/dist/index.css';
 
 const IngredientsList = () => {
     const dispatch = useDispatch();
     const ingredientsByCategoryList = useSelector((state) => state.IngredientsByCategory);
 
-    let {id} = useParams();
+    let { id } = useParams();
 
     useEffect(() => {
         const fetchData = () => {
-          dispatch(getIngredientByCategory(id));
+            dispatch(getIngredientByCategory(id));
         };
         fetchData();
-      }
+    }
         , [dispatch])
 
-        const showData = () => {
-            if (ingredientsByCategoryList.data.length > 0) {
-              return ingredientsByCategoryList.data.map((element) => <p>{element.libelle} </p>);
-        
-            } else {
-              if (ingredientsByCategoryList.loading) {
+    const showData = () => {
+        if (ingredientsByCategoryList.data.length > 0) {
+            return <ReactFlexyTable data={ingredientsByCategoryList.data} /> 
+            //ingredientsByCategoryList.data.map((element) => <p>{element.libelle} </p>);            
+        } else {
+            if (ingredientsByCategoryList.loading) {
                 return <Loading />;
-              }
-              if (ingredientsByCategoryList.errorMsg !== "") {
+            }
+            if (ingredientsByCategoryList.errorMsg !== "") {
                 return <p>{ingredientsByCategoryList.errorMsg}</p>;
-              }
-        
-              return <p>La catégorie ne contient pas d'ingrédients. </p>;
-            };
-          }
-    
-    
-        return(
-            <div>
+            }
+
+            return <p>La catégorie ne contient pas d'ingrédients. </p>;
+        };
+    }
+
+    return (
+        <div>
             {
                 showData()
             }
-            </div>
-        )
-    
+        </div>
+    )
+
 
 }
 
