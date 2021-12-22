ea6d522a50159d3ebdbb771cb5afdc060dab0330
clarencerouvel <clarence.rouvel@acelys.fr>
2021-12-22 13:56:31 +0100

    add steps & progression avancement #3

--- src/components/ProgressionCreation/ProgressionCreation.js
+++ src/components/ProgressionCreation/ProgressionCreation.js
@@ -11,6 +11,7 @@ import {
 } from "react-router-dom";
 import { useFormik } from 'formik';
 import BackButtonTechnichalSheet from '../BackButtonTechnichalSheet/BackButtonTechnichalSheet';
+import { AiFillCheckCircle } from 'react-icons/ai';
 
 const ProgressionCreation = () => {
     let { nomRecette } = useParams();
@@ -57,6 +58,10 @@ const ProgressionCreation = () => {
         setAddMoreStep(true);
     }
 
+    const displayInfo4 = () => {
+        setAddingStepFinished(true);
+    }
+
     const CreationProgression = () => {
         const formik = useFormik({
             initialValues: {
@@ -73,7 +78,7 @@ const ProgressionCreation = () => {
                     </div>
                     <form onSubmit={formik.handleSubmit}>
                         <div className="container-input1">
-                            <div className="sub-container">
+                            <div className="sub-container2">
                                 <label htmlFor="ReferenceProgression">Référence de la progression</label>
                                 <input
                                     id="ReferenceProgression"
@@ -112,107 +117,104 @@ const ProgressionCreation = () => {
         });
         return (
             <>
-                <div className="container mt-3 mb-2 text-center" >
-                    <h1>Progression : {reference}</h1>
-                </div>
-                {!stepCreated ? <div className="container mt-3 mb-2" >
-                    {/*addMoreStep ?
+                {!stepCreated && !addMoreStep ?
+                    <div className="container mt-3 mb-2 text-center" >
+                        <Button type="button" size="lg" className="button-disabled mt-3" disabled>
+                            <div><AiFillCheckCircle />Progression ajoutée</div>
+                        </Button>
+                    </div> : null}
+
+                {!stepCreated ?
+                    <>
                         <div className="container mt-3 mb-2" >
-                            <div className="text-center">
-                                <h2>Ajouter une autre étape ...</h2>
+                            <div className="text-center mb-3">
+                                <h1>Progression : {reference}</h1>
+                                <h2 className="mt-3">Ajoutez des étapes à la progression</h2>
                             </div>
+                            <form onSubmit={formik.handleSubmit}>
 
+                                <div className="sub-container">
+
+                                    <label htmlFor="TitreEtape">Titre de l'étape</label>
+                                    <input
+                                        id="TitreEtape"
+                                        name="TitreEtape"
+                                        type="text"
+                                        onChange={(event) => {
+                                            setTitre(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="input3"
+                                    />
+                                    {formik.errors.TitreEtape ? (
+                                        <div className="erreur">{formik.errors.TitreEtape}</div>
+                                    ) : null}
+                                    <label htmlFor="DescriptionEtape">Description de l'étape</label>
+                                    <textarea
+                                        id="DescriptionEtape"
+                                        name="TitreEtape"
+                                        onChange={(event) => {
+                                            setDescription(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="longinput"
+                                    />
+                                    <label htmlFor="Temps" className="mt-2">Temps (en min)</label>
+                                    <input
+                                        id="Temps"
+                                        name="Temps"
+                                        type="number"
+                                        min="1"
+                                        onChange={(event) => {
+                                            setTemps(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="input3"
+                                    />
+                                    {formik.errors.Temps ? (
+                                        <div className="erreur">{formik.errors.Temps}</div>
+                                    ) : null}
+                                    <label htmlFor="Ordre" className="mt-2">Ordre</label>
+                                    <input
+                                        id="Ordre"
+                                        name="Ordre"
+                                        type="number"
+                                        min="1"
+                                        onChange={(event) => {
+                                            setOrdre(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="input3"
+                                    />
+                                    {formik.errors.Ordre ? (
+                                        <div className="erreur">{formik.errors.Ordre}</div>
+                                    ) : null}
+                                    {reference && titre && temps && ordre ? <Button type="button" size="lg" onClick={displayInfo2} className="step-create mt-3"><div>Créer l'étape</div></Button>
+                                        : <Button type="button" size="lg" className="step-create mt-3" disabled><div>Ajouter l'étape</div></Button>}
+                                </div>
+                            </form>
                         </div>
-                    : null*/}
-
-                    <div className="text-center">
-
-                        {!addMoreStep ?
-                            <h2>Ajoutez des étapes à la progression</h2> : null}
-
-                    </div>
-                    <form onSubmit={formik.handleSubmit}>
-
-                        <div className="sub-container">
-
-                            <label htmlFor="TitreEtape">Titre de l'étape</label>
-                            <input
-                                id="TitreEtape"
-                                name="TitreEtape"
-                                type="text"
-                                onChange={(event) => {
-                                    setTitre(event.target.value);
-                                }}
-                                onBlur={formik.handleBlur}
-                                className="input3"
-                            />
-                            {formik.errors.TitreEtape ? (
-                                <div className="erreur">{formik.errors.TitreEtape}</div>
-                            ) : null}
-                            <label htmlFor="DescriptionEtape">Description de l'étape</label>
-                            <textarea
-                                id="DescriptionEtape"
-                                name="TitreEtape"
-                                onChange={(event) => {
-                                    setDescription(event.target.value);
-                                }}
-                                onBlur={formik.handleBlur}
-                                className="longinput"
-                            />
-                            <label htmlFor="Temps" className="mt-2">Temps (en min)</label>
-                            <input
-                                id="Temps"
-                                name="Temps"
-                                type="number"
-                                min="1"
-                                onChange={(event) => {
-                                    setTemps(event.target.value);
-                                }}
-                                onBlur={formik.handleBlur}
-                                className="input3"
-                            />
-                            {formik.errors.Temps ? (
-                                <div className="erreur">{formik.errors.Temps}</div>
-                            ) : null}
-                            <label htmlFor="Ordre" className="mt-2">Ordre</label>
-                            <input
-                                id="Ordre"
-                                name="Ordre"
-                                type="number"
-                                min="1"
-                                onChange={(event) => {
-                                    setOrdre(event.target.value);
-                                }}
-                                onBlur={formik.handleBlur}
-                                className="input3"
-                            />
-                            {formik.errors.Ordre ? (
-                                <div className="erreur">{formik.errors.Ordre}</div>
-                            ) : null}
-                            {reference && titre && temps && ordre ? <Button type="button" size="lg" onClick={displayInfo2} className="step-create mt-3"><div>Créer l'étape</div></Button>
-                                : <Button type="button" size="lg" className="step-create mt-3" disabled><div>Ajouter l'étape</div></Button>}
-                        </div>
-                    </form>
-                </div> : null}
+                    </> : null}
             </>);
     }
 
     const AddMoreSteps = () => {
         return (
             <>
-                {/*<div className="container mt-3 mb-2" >
-                     <div className="text-center">
-                             <h1>Ajouter d'autres étapes ?</h1>
-                         </div>
-     
-        </div>*/}
-                <div className="container mt-3 mb-2" >
-                    <div className="text-center">
-                        <h2>Voulez-vous ajouter une autre étape ?</h2>
-                        <button class="btn btn-primary btn-lg active" onClick={displayInfo3}>Oui</button>
-                        <button class="btn btn-success btn-lg">Terminer</button>
-                    </div>
+                <div className="container mt-3 mb-2 text-center" >
+                    <h1>Progression : {reference}</h1>
                 </div>
+                {!addingStepFinished ?
+                    <div className="container mt-3 mb-2 p-5" >
+                        <div className="text-center">
+                            <h1>Voulez-vous ajouter une autre étape ?</h1>
+                            <div className="mt-4">
+                                <button className="btn btn-primary btn-lg m-2" onClick={displayInfo3}>Créer une autre étape</button>
+                                <button className="btn btn-success btn-lg m-2" onClick={displayInfo4}>Terminer l'ajout d'étape</button>
+                            </div>
+                        </div>
+                    </div> : null}
+
             </>
         );
     }
@@ -220,7 +222,12 @@ const ProgressionCreation = () => {
     const AddIngredients = () => {
         return (
             <>
-                <div>Ajout ingredients</div>
+                <div className="container mt-3 mb-2 text-center" >
+                    <h2>Associez des ingrédients aux étapes créées</h2>
+                </div>
+                <div className="container mt-3 mb-2 text-center" >
+                    
+                </div>
             </>
         )
     }
