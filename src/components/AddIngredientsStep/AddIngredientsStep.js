5dcca4181f2f5886dc2dfe1973d08d41e5b7eda3
clarencerouvel <clarence.rouvel@acelys.fr>
2021-12-23 11:07:56 +0100

    add ingredients in steps & progression avancement #3

--- src/components/AddIngredientsStep/AddIngredientsStep.js
+++ src/components/AddIngredientsStep/AddIngredientsStep.js
@@ -25,12 +25,16 @@ const AddIngredientsStep = () => {
     const options = [{ value: '1', label: 'oui' }, { value: '2', label: "tt" }];
     const [ingredientAdded, setIngredientAdded] = useState(false);
     console.log(ingredientAdded);
-    const displayInfo5 = () => {       
+    const displayInfo5 = () => {
         setAddIngredient(true);
     }
 
     const displayInfo6 = () => {
         setIngredientAdded(true);
+    }
+
+    const displayInfo7 = () => {
+        setIngredientAdded(false);
     }
 
     const validate = () => {
@@ -70,82 +74,94 @@ const AddIngredientsStep = () => {
             <div className="container mt-3 mb-2" >
 
                 {!ingredientAdded ?
-                (!addIngredient ?
+                    (!addIngredient ?
+                        <>
+                            <h3 className="text-center mb-2">Associez une liste d'ingrédients à une étape</h3>
+                            <div className="container-input1">
+                                <div className="sub-container2">
+                                    <label htmlFor="NomListe">Nom de la liste d'ingrédients ou de l'étape</label>
+                                    <input
+                                        id="NomListe"
+                                        name="NomListe"
+                                        type="text"
+                                        onChange={(event) => {
+                                            setNomListe(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="inputname"
+                                    />
+                                    {formik.errors.NomListe ? (
+                                        <div className="erreur">{formik.errors.NomListe}</div>
+                                    ) : null}
+                                </div>
+                                <div className='sub-container3 m-2'>
+                                    {nomListe ? <Button type="button" onClick={displayInfo5} className="step-create mt-3"><div>Créer Liste d'ingrédients</div></Button>
+                                        : <Button type="button" className="step-create mt-3" disabled><div>Créer Liste d'ingrédients</div></Button>}
+                                </div>
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div className="text-center mb-3">
+                                <h3 className="mt-1">Associez les ingrédients pour : {nomListe}</h3>
+                            </div>
+                            <h3 className="mt-4">Ajouter un ingrédient</h3>
+                            <div className="container2 mt-3">
 
-                    <div className="container-input1">
-
-                        <div className="sub-container2">
-                            <label htmlFor="NomListe">Nom de la liste d'ingrédients ou de l'étape</label>
-                            <input
-                                id="NomListe"
-                                name="NomListe"
-                                type="text"
-                                onChange={(event) => {
-                                    setNomListe(event.target.value);
-                                }}
-                                onBlur={formik.handleBlur}
-                                className="inputname"
-                            />
-                            {formik.errors.NomListe ? (
-                                <div className="erreur">{formik.errors.NomListe}</div>
-                            ) : null}
-                        </div>
-                        <div className='sub-container3 m-2'>
-                            {nomListe ? <Button type="button" onClick={displayInfo5} className="step-create mt-3"><div>Créer Liste d'ingrédients</div></Button>
-                                : <Button type="button" className="step-create mt-3" disabled><div>Créer Liste d'ingrédients</div></Button>}
-                        </div>
-                    </div>
-
-                    :
-                    <>
+                                <div className="sub-container2 mt-1">
+                                    <label htmlFor="NomIngredient">Libellé de l'ingrédient</label>
+                                    <Select
+                                        options={options}
+                                        className="select-search"
+                                        id="NomIngredient"
+                                        name="NomIngredient"
+                                        onChange={(event) => {
+                                            setLibelleIngredient(event.label);
+                                        }}
+                                        onBlur={formik.handleBlur} />
+                                    {formik.errors.NomIngredient ? (
+                                        <div className="erreur">{formik.errors.NomIngredient}</div>
+                                    ) : null}
+                                </div>
+                                <div className="sub-container4">
+                                    <label htmlFor="Quantite">Quantité (cf. unité de l'ingrédient)</label>
+                                    <input
+                                        id="Quantite"
+                                        name="Quantite"
+                                        type="number"
+                                        min="0"
+                                        step="0.01"
+                                        onChange={(event) => {
+                                            setQuantite(event.target.value);
+                                        }}
+                                        onBlur={formik.handleBlur}
+                                        className="inputquantite"
+                                    />
+                                    {formik.errors.Quantite ? (
+                                        <div className="erreur">{formik.errors.Quantite}</div>
+                                    ) : null}
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                {quantite && libelleIngredient ? <Button type="button" size="lg" onClick={displayInfo6} className="step-create mt-1"><div>Ajouter l'ingrédient</div></Button>
+                                    : <Button type="button" size="lg" className="step-create mt-1" disabled><div>Ajouter l'ingrédient</div></Button>}
+                            </div>
+                        </>
+                    ) :
+                    null}
+                {
+                    ingredientAdded ?
                         <div className="text-center mb-3">
                             <h3 className="mt-1">Associez les ingrédients pour : {nomListe}</h3>
-                        </div>
-                        <h3 className="mt-4">Ajouter un ingrédient</h3>
-                        <div className="container2 mt-3">
-
-                            <div className="sub-container2 mt-1">
-                                <label htmlFor="NomIngredient">Libellé de l'ingrédient</label>
-                                <Select
-                                    options={options}
-                                    className="select-search"
-                                    id="NomIngredient"
-                                    name="NomIngredient"
-                                    onChange={(event) => {
-                                        setLibelleIngredient(event.label);
-                                    }}
-                                    onBlur={formik.handleBlur} />
-                                {formik.errors.NomIngredient ? (
-                                    <div className="erreur">{formik.errors.NomIngredient}</div>
-                                ) : null}
+                            <h3>Voulez-vous ajouter un autre ingrédient ?</h3>
+                            <div className="mt-4">
+                                 <button className="btn btn-primary btn-lg m-2" onClick={displayInfo7}>Ajout d'un autre ingrédient</button>
+                                {/*<Link to={"/sheets/creation/" + nomRecette + "/" + reference} >
+                                <button className="btn btn-success btn-lg m-2" >Terminer l'ajout d'étape</button>
+                                    </Link>*/}
                             </div>
-                            <div className="sub-container4">
-                                <label htmlFor="Quantite">Quantité (cf. unité de l'ingrédient)</label>
-                                <input
-                                    id="Quantite"
-                                    name="Quantite"
-                                    type="number"
-                                    min="0"
-                                    step="0.01"
-                                    onChange={(event) => {
-                                        setQuantite(event.target.value);
-                                    }}
-                                    onBlur={formik.handleBlur}
-                                    className="inputquantite"
-                                />
-                                {formik.errors.Quantite ? (
-                                    <div className="erreur">{formik.errors.Quantite}</div>
-                                ) : null}
-                            </div>
-
-                        </div>
-                        <div className="text-center">
-                            {quantite && libelleIngredient ? <Button type="button" size="lg" onClick={displayInfo6} className="step-create mt-1"><div>Ajouter l'ingrédient</div></Button>
-                                : <Button type="button" size="lg" className="step-create mt-1" disabled><div>Ajouter l'ingrédient</div></Button>}
-                        </div>
-                    </>
-    ) : <div>oui</div>}
-               
+                        </div> : null
+                }
             </div>
         </>
     );
