0cd73d2e6a1cecc85ea0a9e1ff5296515dc3c394
clarencerouvel <clarence.rouvel@acelys.fr>
2021-12-23 13:26:10 +0100

    add ingredients in steps & progression avancement #5

--- src/components/FinishAddIngredientsStep/FinishAddIngredientsStep.js
+++ src/components/FinishAddIngredientsStep/FinishAddIngredientsStep.js
@@ -20,8 +20,27 @@ const FinishAddIngredientsStep = () => {
     let { nomRecette, nomProgression } = useParams();
 
     return (
-        
-        <div>finish add ingredients ?</div>
+        <>
+            <BackButtonTechnichalSheet />
+            <div className="container mt-3 mb-2 text-center" >
+                <h1>Progression : {nomProgression}</h1>
+            </div>
+            <div className="container mt-3 mb-2 text-center" >
+                <h2>Associez des ingrédients aux étapes créées</h2>
+            </div>
+            <div className="container mt-3 mb-2 text-center" >
+                <div className="text-center mb-3">
+                    <h3 className="mb-2">Associez une liste d'ingrédients à une étape</h3>
+                    <h3>Voulez-vous ajouter une autre liste d'ingrédients ?</h3>
+                    <div className="mt-4">
+                        <Link to={"/sheets/creation/" + nomRecette + "/" + nomProgression} >
+                            <button className="btn btn-primary btn-lg m-2">Ajout d'une autre liste</button>
+                        </Link>
+                        {/* <button className="btn btn-success btn-lg m-2" onClick={displayInfo8}>Terminer l'ajout de listes</button> */}
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
